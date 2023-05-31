ca52f9d06849be996f9a96a92f6f18e52d09ce2c
cviguers <cviguers@gmail.com>
2023-05-31 13:53:38 -0400

    prepped for l/d mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,21 @@
 import React from 'react'; // Importing React library
+import '../styles/footer.css'; // Importing the stylesheets
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importing FontAwesome Icons
-import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'; // Importing specific icons used below
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Importing specific icons used below
 
 const Footer = () => { 
   return (
-    <footer className="bg-black text-white py-3 footer"> {/* Rendering the footer */}
+    <footer className="py-3 footer"> {/* Rendering the footer */}
       <div className="container">
         <div className="row">
           <div className="col-12 text-center">
-            <p className="footerText">&copy; Made by: Christopher Snyder</p>
-            <div className="social-icons">  {/* Rendering a FontAwesomeIcon with the GitHub, LinkedIn or Facebook icon */}
-              <a href="https://github.com/csnyder94"> 
-                <FontAwesomeIcon icon={faGithub} style={{fontSize: '34px'}}/> 
+            <p className="footerText">&copy; Made with ❤ by: caro vigies</p>
+            <div className="social-icons">  {/* Rendering a FontAwesomeIcon with the GitHub or LinkedIn icon */}
+              <a href="https://github.com/cviguers"> 
+                <FontAwesomeIcon icon={faGithub} className='icon' style={{fontSize: '34px'}}/> 
               </a>
-              <a href="https://www.linkedin.com/in/christopher-snyder-a6000a246/"> 
-                <FontAwesomeIcon icon={faLinkedin} style={{fontSize: '34px'}} className="px-3"/> 
-              </a>
-              <a href="https://www.facebook.com/chris.snyder.792"> 
-                <FontAwesomeIcon icon={faFacebook} style={{fontSize: '34px'}} /> 
+              <a href="https://www.linkedin.com/in/caroline-viguers-1568531ba/"> 
+                <FontAwesomeIcon icon={faLinkedin} style={{fontSize: '34px'}} className="px-3 icon"/> 
               </a>
             </div>
           </div>
