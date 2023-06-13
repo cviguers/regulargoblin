a2bc7f7eb7cc3febece4ee8659dd8c0e812dbd61
cviguers <cviguers@gmail.com>
2023-06-13 10:59:28 -0400

    toggle light/dark theme finally working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ColorModeScript } from "@chakra-ui/react";
 import App from './App';
+import theme from "./theme";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App/>
+    </React.StrictMode>
+  </ChakraProvider>
+);
